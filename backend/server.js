9c951fdeefb87b9dc2c116e5c1a057fc781dc61c
Koziolek <koziolola43@gmail.com>
2025-12-08 20:54:29 +0100

    feat: render deployment config

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,30 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const __dirname = path.resolve();
 
 app.use(express.json()); // parse HTTP body
 app.use(cors());
 app.use(helmet()); // middleware that sets various HTTP headers to protect your app from common web vulnerabilities
 app.use(morgan("dev")); // log the requests
 
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+    console.log("on prod")
+}
+
 app.listen(PORT, () =>{
     console.log("Server is running on " + PORT);
 });
 
 // npm run dev - start server
+// npm start - backend and frontend running on the same port (localhost:PORT)
