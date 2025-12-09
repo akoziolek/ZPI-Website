5707634774908ec7a76451858f230373067567e4
Koziolek <koziolola43@gmail.com>
2025-12-09 08:09:21 +0100

    feat: backend npm

--- backend/server.js
+++ backend/server.js
@@ -9,20 +9,12 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const __dirname = path.resolve();
 
 app.use(express.json()); // parse HTTP body
-app.use(cors());
+app.use(cors()); 
 app.use(helmet()); // middleware that sets various HTTP headers to protect your app from common web vulnerabilities
 app.use(morgan("dev")); // log the requests
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-    app.get("/{*any}", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    });
-    console.log("on prod")
-}
 
 app.listen(PORT, () =>{
     console.log("Server is running on " + PORT);
